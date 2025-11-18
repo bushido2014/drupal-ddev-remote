e5bed5c0fe3eaa41bb007736cec346a6dabbe23a
Viorel Soltan <joomjoom2011@gmail.com>
2025-11-18 15:46:22 +0000

    add swiper and integrate in template

---
+++ drupal/web/themes/custom/dnorthman/js/main.js
@@ -0,0 +1,26 @@
+/**
+ * @file
+ * dnorthman theme JavaScript.
+ */
+
+(function (Drupal, once) {
+  "use strict";
+
+
+  const swiper = new Swiper('.testimonial-slider', {
+    loop:true,
+    slidesPerView: 1,
+  });
+
+
+
+  Drupal.behaviors.dnorthman = {
+    attach: function (context, settings) {
+      // Restul comportamentului tău
+      if (!document.body.classList.contains("dnorthman-loaded")) {
+        console.log("🎨 Dnorthman theme loaded successfully!");
+        document.body.classList.add("dnorthman-loaded");
+      }
+    },
+  };
+})(Drupal, once);
